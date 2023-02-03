679e644d89edbf1e93e027bd9fb2abaf6aac9e6d
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-02-03 16:46:05 -0500

    Add ability to input values to new matrix, needs error handling

--- Matrix.java
+++ Matrix.java
@@ -9,10 +9,11 @@ public class Matrix{
             try{
                 System.out.println("Enter number of rows: ");
                 row = n.nextInt();
+                if(row <= 0) throw new IllegalArgumentException();
                 validInput = true;
             }
             catch(IllegalFormatException e){
-                System.out.println("not a whole number. try again");
+                System.out.println("not a proper value. try again");
             }
         }
         validInput = false;
@@ -20,14 +21,21 @@ public class Matrix{
             try{
                 System.out.println("Enter number of columns: ");
                 column = n.nextInt();
+                if(column <= 0) throw new IllegalArgumentException();
                 validInput = true;
             }
             catch(IllegalFormatException e){
-                System.out.println("not a whole number. try again");
+                System.out.println("not a proper value. try again");
             }
         } //End of both try/while blocks for taking in input for matrix size
-        table = new double[row][column];
-
+        table = new double[row-1][column-1];
+        for(int i = 0; i > row; i++){ //Loop to fill in matrix values.
+            for(int j = 0; j > column; j++){
+                System.out.println("Type a value for for element "+i+", "+ j);
+                table[i][j] = n.nextDouble();
+                matrixPrint(this.table);
+            }
+        }
         n.close();
     }
     public static void matrixPrint(double[][] tableIn){
